0d7d1094feb87370405385d7f4abfca28bc68054
Ginezis <Ginezis25@gmail.com>
2023-11-07 17:51:25 +0300

    fix: update config and fix bugs with husky

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,7 +4,7 @@ module.exports = {
     browser: true,
     es6: true,
     jest: true,
-    node: true
+    node: true,
   },
   extends: [
     'eslint:recommended',
@@ -16,43 +16,38 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
-      jsx: true
+      jsx: true,
     },
     ecmaVersion: 'latest',
-    sourceType: 'module'
+    sourceType: 'module',
   },
   ignorePatterns: ['dist', '.eslintrc.cjs'],
-  plugins: [
-    'react',
-    'react-hooks',
-    'react-refresh',
-    '@typescript-eslint',
-  ],
+  plugins: ['react', 'react-hooks', 'react-refresh', '@typescript-eslint'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
-    "react-hooks/rules-of-hooks": "error",
-    "react-hooks/exhaustive-deps": "warn",
-    "comma-dangle": ["error", "only-multiline"],
-    "react/prop-types": "off",
-    "react/display-name": "off",
-    "@typescript-eslint/interface-name-prefix": "off",
-    "@typescript-eslint/ban-ts-comment": "error",
-    "@typescript-eslint/no-non-null-assertion": "off",
-    "@typescript-eslint/explicit-module-boundary-types": "off",
-    "@typescript-eslint/no-empty-function": "off",
-    "@typescript-eslint/no-explicit-any": "error",
-    "@typescript-eslint/no-var-requires": "off",
-    "react/jsx-uses-react": "off",
-    "react/react-in-jsx-scope": "off"
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'comma-dangle': ['error', 'only-multiline'],
+    'react/prop-types': 'off',
+    'react/display-name': 'off',
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/ban-ts-comment': 'error',
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-empty-function': 'off',
+    '@typescript-eslint/no-explicit-any': 'error',
+    '@typescript-eslint/no-var-requires': 'off',
+    'react/jsx-uses-react': 'off',
+    'react/react-in-jsx-scope': 'off',
   },
 
   settings: {
     react: {
-      pragma: "React",
-      version: "detect"
-    }
-  }
-}
+      pragma: 'React',
+      version: 'detect',
+    },
+  },
+};
